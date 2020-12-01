04c4843fb87da09067484aa135ce1e54965ac711
himanshuGaur684 <60474413+himanshuGaur684@users.noreply.github.com>
2020-12-02 01:06:09 +0530

    App Intro Screen

--- app/src/main/java/gaur/himanshu/august/viewpager2/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/august/viewpager2/MainActivity.kt
@@ -1,6 +1,7 @@
 package gaur.himanshu.august.viewpager2
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -14,25 +15,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val viewpager2= findViewById<ViewPager2>(R.id.view_pager_2)
-        val tablayout= findViewById<TabLayout>(R.id.tab_layout)
+       // val tablayout= findViewById<TabLayout>(R.id.tab_layout)
 
 
         viewpager2.adapter=ViewPagerAdapter(this.supportFragmentManager,lifecycle)
-        TabLayoutMediator(tablayout,viewpager2){tab,position->
 
-            when(position){
-                0->{
-                    tab.text="First"
-                }
-                1->{
-                    tab.text="Second"
-                }
-                2->{
-                    tab.text="Third"
-                }
-            }
 
-        }.attach()
+
+//        TabLayoutMediator(tablayout,viewpager2){tab,position->
+//
+//            when(position){
+//                0->{
+//                    tab.text="First"
+//                }
+//                1->{
+//                    tab.text="Second"
+//                }
+//                2->{
+//                    tab.text="Third"
+//                }
+//            }
+//
+//        }.attach()
 
 
     }
